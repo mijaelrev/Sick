49b09b745d6022e287cd87403513a983423cf3c8
Sleider <rpedrojunior6@gmail.com>
2021-04-19 20:16:48 -0300

    commid medio dia jornada de trabajo XD

--- Assets/_Scripts/Interactuable/Items/Item.cs
+++ Assets/_Scripts/Interactuable/Items/Item.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public class Item : MonoBehaviour, IItem
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Atack()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if(coll.CompareTag("Player"))
+        {
+            coll.gameObject.GetComponent<Inventari>().AddInFloor(this);
+        }
+    }
+
+    private void OnTriggerExit(Collider coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            coll.gameObject.GetComponent<Inventari>().RemoveInFloor(this);
+        }
     }
 }
---
+++ Assets/_Scripts/Player/Inventari.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventari : MonoBehaviour
+{
+    int selected;
+    LinkedList<IItem> inFloor;//en el suelo
+    [SerializeField] IItem[] items;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Interact();
+    }
+
+    void Interact()
+    {
+        if(inFloor.Count > 0 && Input.GetKeyDown(KeyCode.E))
+        {
+            
+        }
+    }
+
+    void Take()
+    {
+
+    }
+
+    void Drop()
+    {
+
+    }
+
+    public void AddInFloor(IItem item) => inFloor.AddLast(item);//agregar objetos a la lista
+    public void RemoveInFloor(IItem item) => inFloor.Remove(item);//remover objetos de la lista
+}
